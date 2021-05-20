59dda24e85e8c8bf79604e4e3c56b8c8cdf0bc00
peytonkemp <peyton.kemp@gmail.com>
2021-05-20 10:36:23 -0500

    committing to pull down new main

--- src/scripts/message/MessageForm.js
+++ src/scripts/message/MessageForm.js
@@ -1,1 +1,20 @@
-import { getMessages } from "../data/provider.js";
+import { getMessages, getUsers } from "../data/provider.js";
+
+const users = getUsers()
+
+export const messageForm = () => {
+    return `
+        <div class="messageForm">
+            <form>
+                <fieldset>
+                    <lable></lable>
+                    <input type="text" name="email" autofocus placeholder="Email address" />
+                </fieldset>
+                <fieldset>
+                    <lable for="message">Message:</lable>
+                    <input type="text" name="text" autofocus placeholder="Enter message here" />
+                </fieldset>
+            </form>
+        </div>
+    `
+}
