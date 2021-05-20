5fc7bb3c73e4b3ce54615ffeb188d9c8a7ab75cf
peytonkemp <peyton.kemp@gmail.com>
2021-05-20 14:22:43 -0500

    postList component doing what it's supposed to do:)

--- src/scripts/feed/PostList.js
+++ src/scripts/feed/PostList.js
@@ -1,9 +1,12 @@
 import { getPosts } from "../data/provider.js";
+import { Post } from "./Post.js";
 
 
 export const PostList = () => {
     const posts = getPosts()
 
-    
+    return `
+    ${posts.map(Post).join("")}
+    `
 
 }
