8a3ef6ddc511f5d2321dccf96ac72a5ec7abf36b
peytonkemp <peyton.kemp@gmail.com>
2021-05-20 13:27:59 -0500

    finished building out direct message form

--- src/scripts/message/MessageForm.js
+++ src/scripts/message/MessageForm.js
@@ -1,20 +1,27 @@
-import { getMessages, getUsers } from "../data/provider.js";
+import { getUsers } from "../data/provider.js";
 
-const users = getUsers()
 
-export const messageForm = () => {
+
+export const MessageForm = () => {
+    const users = getUsers()
     return `
-        <div class="messageForm">
-            <form>
-                <fieldset>
-                    <lable></lable>
-                    <input type="text" name="email" autofocus placeholder="Email address" />
-                </fieldset>
-                <fieldset>
-                    <lable for="message">Message:</lable>
-                    <input type="text" name="text" autofocus placeholder="Enter message here" />
-                </fieldset>
-            </form>
+        <div class="directMessage">
+            <div class="dropdown select--friends">
+                <lable>Recipient:</lable>
+                <select class="select--friends">
+                    <option value="default">Select a user...</option>
+                    ${users.map(u => {
+                        return `<option value="user--${u.id}">${u.name}</option>`
+                    }).join("")}
+                </select>   
+            </div>
+            <div class="message__section">
+                <lable for="message">Message:</lable>
+                <input type="text" name="text" autofocus placeholder="Enter message here" />
+            </div>
+            <button id="directMessage__submit">Save</button>
+            <button id="directMessage__cancel">Cancel</button>
+            <button id="directMessage__close">x</button>
         </div>
     `
 }
