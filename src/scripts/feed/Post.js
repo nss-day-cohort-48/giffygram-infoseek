b12cb8601bcefac05cfb4d01f6d6554a92e92220
hann <hmweeks93@gmail.com>
2021-05-20 12:02:06 -0500

    fixed errors on post.js

--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -1,22 +1,23 @@
-import { getUsers, getPosts } from "./data/provider.js"
-
-const users = getUsers
-const posts = getPosts
+import { getUsers } from "../data/provider.js"
 
 // function that returns html for post
 
 
 
-export const post = () => {
+export const Post = (postObject) => {
 
-    let post = posts.map(post => post.id);
-    let user = users.map(user => user.name);
-   
-   postHTML = `
+    const users = getUsers()
+   let foundUser = users.find(userObject => {
+       if (userObject.id === postObject.userId){
+           return userObject
+       }
+   })
+
+  let postHTML = `
         <div class="post"> 
-        <h2>${post.title}</h2>
-        <img src="${gifUrl}" alt="${discription}"/>
-        posted by ${user} on ${timestamp}
+        <h2>${postObject.title}</h2>
+        <img src="${postObject.gifUrl}" alt="${postObject.discription}"/>
+        posted by ${foundUser.name} on ${postObject.timestamp}
         </div>
         `
     return postHTML
