ebeb7237bcc8ed08d7f1e0e6775547ab61b8d133
Madeline Crowley <madelinecrowley@gmail.com>
2021-05-20 13:53:59 -0500

    NavBar html component added

--- src/scripts/GiffyGram.js
+++ src/scripts/GiffyGram.js
@@ -1,9 +1,11 @@
+import { NavBar } from "./nav/NavBar.js";
 import { MessageForm } from "./message/MessageForm.js"
 
 export const GiffyGram = () => {
 
     // Show main main UI
-    return `<h1>Giffygram</h1>
+    return `
+    ${NavBar()}
     ${MessageForm()}
     `
 }
--- src/scripts/nav/NavBar.js
+++ src/scripts/nav/NavBar.js
@@ -3,9 +3,21 @@
 export const NavBar = () => {
     return `
     <nav class="navigation">
-    <div class="navigation__item navigation__icon">
-    <img src="./images/pb.png" alt="Giffygram icon" id="logo">
-    </div>
+        <div class="navigation__item navigation__icon">
+            <img src="./images/pb.png" alt="Giffygram icon" id="logo">
+        </div>
+        <div class="navigation__item navigation__name">
+            Giffygram
+        </div>
+        <div class="navigation__item navigation__message">
+            <img id="directMessageIcon" src="/images/fountain-pen.svg" alt="Direct message">
+            <div class="notification__count">
+                0
+            </div>
+        </div>
+        <div class="navigation__item navigation__logout">
+            <button id="logout" class="fakeLink">Logout</button>
+        </div>
     </nav>
     `
 }
