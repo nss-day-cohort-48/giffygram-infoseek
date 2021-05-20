a8809fa29007e4fa86471fbde24db84fe0a9b94a
Madeline Crowley <madelinecrowley@gmail.com>
2021-05-20 10:19:30 -0500

    added fetch calls to main.js and stateChanged event listener

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,32 @@
 import { GiffyGram } from "./GiffyGram.js"
 import { LoginForm } from "./auth/Login.js";
+import { fetchFollows, fetchLikes, fetchMessages, fetchPosts, fetchUsers } from "./data/provider.js";
 
 const applicationElement = document.querySelector(".giffygram")
 
 export const renderApp = () => {
-    const user = parseInt(localStorage.getItem("gg_user"))
 
-    if (user) {
-        applicationElement.innerHTML = GiffyGram()
-    } else {
-        applicationElement.innerHTML = LoginForm()
-    }
+    fetchUsers()
+        .then(fetchPosts)
+        .then(fetchLikes)
+        .then(fetchMessages)
+        .then(fetchFollows)
+        .then(
+            () => {
+
+                const user = parseInt(localStorage.getItem("gg_user"))
+
+                if (user) {
+                    applicationElement.innerHTML = GiffyGram()
+                } else {
+                    applicationElement.innerHTML = LoginForm()
+                }
+            }
+        )
 }
 
 renderApp()
+
+applicationElement.addEventListener("stateChanged",
+    customEvent => {renderApp()}
+)
