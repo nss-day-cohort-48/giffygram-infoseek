f8ff6c6b67d83264f2560fa1efccad45227687f9
peytonkemp <peyton.kemp@gmail.com>
2021-05-20 15:54:21 -0500

    added if else to determine if a post is liked or not... still need to find current user

--- src/scripts/feed/Post.js
+++ src/scripts/feed/Post.js
@@ -17,33 +17,53 @@ export const Post = (postObject) => {
 
     const likes = getLikes()
     const likedPost = likes.find(likeObj => {
-        if (postOject.id === likeObj.postId) {
+        if (postObject.id === likeObj.postId) {
             return likeObj
         }
     })
 
-    if (likedPost === undefined) {
-        
-    }
+    let postHTML = ""
 
-  let postHTML = `
+    if (likedPost === undefined) {
+        postHTML = `
         <section class="post"> 
-            <h2 class="post__title">${postObject.title}</h2>
-            <img class="post__image" src="${postObject.gifURL}" alt="${postObject.description}"/>
-            <div class="post__description">
-                ${postObject.description}
-            </div>
-            <div class="post__tagline">
-                Posted by 
-                <a href="#" class="profileLink" id="profile--1">${foundUser.name}</a>
-                on ${new Date(timestamp).toLocaleDateString("en-US")}
-            </div>
-            <div class="post__actions">
-                <div>
-                    <img id="favoritePost--${postObject.id}" class="actionIcon" src="/images/favorite-star-blank.svg"
-                </div>
-            </div>
+        <h2 class="post__title">${postObject.title}</h2>
+        <img class="post__image" src="${postObject.gifURL}" alt="${postObject.description}"/>
+        <div class="post__description">
+        ${postObject.description}
+        </div>
+        <div class="post__tagline">
+        Posted by 
+        <a href="#" class="profileLink" id="profile--1">${foundUser.name}</a>
+        on ${new Date(timestamp).toLocaleDateString("en-US")}
+        </div>
+        <div class="post__actions">
+        <div>
+        <img id="favoritePost--${postObject.id}" class="actionIcon" src="/images/favorite-star-blank.svg"
+        </div>
+        </div>
         </section>
         `
+    } else {
+        postHTML = `
+        <section class="post"> 
+        <h2 class="post__title">${postObject.title}</h2>
+        <img class="post__image" src="${postObject.gifURL}" alt="${postObject.description}"/>
+        <div class="post__description">
+        ${postObject.description}
+        </div>
+        <div class="post__tagline">
+        Posted by 
+        <a href="#" class="profileLink" id="profile--1">${foundUser.name}</a>
+        on ${new Date(timestamp).toLocaleDateString("en-US")}
+        </div>
+        <div class="post__actions">
+        <div>
+        <img id="favoritePost--${postObject.id}" class="actionIcon" src="/images/favorite-star-yellow.svg"
+        </div>
+        </div>
+        </section>
+        `
+    }
     return postHTML
 }
