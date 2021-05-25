6796268bdf36d1adf35b7c99f0c0c1c8459d676d
hann <hmweeks93@gmail.com>
2021-05-25 10:12:44 -0500

    messageList function now displays current user messages

--- src/scripts/message/MessageList.js
+++ src/scripts/message/MessageList.js
@@ -1,14 +1,24 @@
 import { getMessages } from "../data/provider.js";
 import { DirectMessage } from "../friends/DirectMessage.js";
-
-
+import { getCurrentUser } from "../data/provider.js";
 
 
 export const MessageList = () => {
+  
+    const currentUser = getCurrentUser()
     const messages = getMessages()
     
-    return `
-    ${messages.map(DirectMessage).join("")}
-    `
 
+    const currentUserMessages = messages.filter(message => { 
+        if (message.recipientId === currentUser) {
+            return true
+        }
+
+    }
+    )
+    const messageArrayOfStrings = currentUserMessages.map(messageObject => {
+        const messageHTML = DirectMessage(messageObject)
+        return messageHTML 
+    })
+    return messageArrayOfStrings.join("")
 }
